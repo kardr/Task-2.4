2caf2cf401249660a4f958a297c1186f551c4455
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-08 12:22:28 +0300

    Добавьте файлы проекта.

---
+++ Task 2.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f=3;
+            int n = 5;
+            for(int i =0;i<10; i++)
+            {
+                int k = f;
+                for(int j = 0; j < n; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write("2" + " ");
+                    }
+                    else Console.Write((k++) + " ");
+                }
+                if (i % 2 != 0)
+                {
+                    f --;  n--;
+                }
+              
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
